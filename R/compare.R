eca08f8db437d59284df30623f55f87dd3a2bbfd
Roland Schmid <roland.schmid@mirai-solutions.com>
2018-07-10 13:52:11 +0200

    added alternative to start file search in same directory

--- R/compare.R
+++ R/compare.R
@@ -15,3 +15,23 @@ compare_with <- function() {
   ret <- system2("meld", args = c(file_1, file_2), wait = FALSE)
   invisible(ret)
 }
+
+
+#' @rdname compare_with
+#' @details compare_with_neighbor starts the file search in the directory of the
+#' first file, while compare_with starts the file search in the project root
+#' directory
+#' @export
+#'
+#' @examples
+#' \dontrun{compare_with_neighbor()}
+compare_with_neighbor <- function() {
+  file_1 <- normalizePath(
+    rstudioapi::getSourceEditorContext()$path
+  )
+  file_2 <- normalizePath(
+    rstudioapi::selectFile(path = dirname(file_1), filter = "*.*")
+  )
+  ret <- system2("meld", args = c(file_1, file_2), wait = FALSE)
+  invisible(ret)
+}
