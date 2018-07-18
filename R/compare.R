02a16e2447a15f72b73b86e3dba53f5ebd130ddc
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2018-07-18 11:47:19 +0200

    Version control repo comparison + minor refinements

--- R/compare.R
+++ R/compare.R
@@ -1,6 +1,11 @@
 #' Version-control comparison of files
 #'
 #' @return Invisibly returns the result of the meld command.
+#'
+#' @details `compare_with_neighbor` starts the file search in the directory of
+#'   the first file, while `compare_with` starts the file search in the working
+#'   directory.
+#'
 #' @export
 #'
 #' @examples
@@ -12,15 +17,11 @@ compare_with <- function() {
   file_2 <- normalizePath(
     rstudioapi::selectFile(path = ".", filter = "*.*")
   )
-  ret <- system2("meld", args = c(file_1, file_2), wait = FALSE)
-  invisible(ret)
+  compare_meld(file_1, file_2)
 }
 
 
 #' @rdname compare_with
-#' @details compare_with_neighbor starts the file search in the directory of the
-#' first file, while compare_with starts the file search in the project root
-#' directory
 #' @export
 #'
 #' @examples
@@ -32,6 +33,44 @@ compare_with_neighbor <- function() {
   file_2 <- normalizePath(
     rstudioapi::selectFile(path = dirname(file_1), filter = "*.*")
   )
+  compare_meld(file_1, file_2)
+}
+
+
+#' @rdname compare_with
+#'
+#' @details `compare_with_repo` and `compare_project_with_repo` compare the
+#'   current file and RStudio project with the version control repository
+#'   version.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{compare_with_repo()}
+compare_with_repo <- function() {
+  file <- normalizePath(
+    rstudioapi::getSourceEditorContext()$path
+  )
+  compare_meld(file)
+}
+
+
+#' @rdname compare_with
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{compare_project_with_repo()}
+compare_project_with_repo <- function() {
+  project_dir <- rstudioapi::getActiveProject()
+  if (is.null(project_dir)) {
+    stop("No active RStudio project detected.")
+  }
+  compare_meld(project_dir)
+}
+
+
+compare_meld <- function(file_1, file_2 = NULL) {
   ret <- system2("meld", args = c(file_1, file_2), wait = FALSE)
   invisible(ret)
 }
