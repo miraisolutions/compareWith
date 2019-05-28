c299d981199ba67c26d00c24fb0ce6d05c21c0bb
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2019-05-28 19:25:05 +0200

    Review and align stop messages on pkg load (#3).
    * Error statement > Make sure... > Check README.
    * Explicit line breaks, no indention (ugly with line wraps)
    * Angular brackets for links.
    * Simplify status logic.
    * Explicit message before the version.

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,28 @@
+check_readme_msg <- c(
+  "Check instructions in the package README\n",
+  "<https://github.com/miraisolutions/compareWith#installation>."
+)
+
 .onLoad <- function(libname, pkgname) {
-  # Stop loading of pkg if Meld is not installed or is encountering issues
+  # Check Meld version, stop pkg loading if Meld is not installed or there are
+  # issues determining its version
+  cat("Check Meld installation:\n")
   suppressWarnings({
     status <- system2("meld", "--version")
     if (status == 127) {
-      stop("
-           Meld is not installed or not added to the path environment variable.
-           Check instructions on README
-           (https://github.com/miraisolutions/compareWith#Installation).",
-           call. = FALSE)
-    } else if (status > 0 && status != 127) {
-      stop("
-           Existing Meld is not working correctly.
-           Make sure your Meld installation is fully functional. Check instructions on README
-           (https://github.com/miraisolutions/compareWith#Installation).",
-           call. = FALSE)
+      stop(
+        "Unable to detect a Meld installation.\n",
+        "Make sure `meld` is installed and can be called from the command line.\n",
+        check_readme_msg,
+        call. = FALSE
+      )
+    } else if (status != 0) {
+      stop(
+        "Existing Meld installation not working correctly.\n",
+        "Make sure you have a fully functional `meld` installed in your system.\n",
+        check_readme_msg,
+        call. = FALSE
+      )
     }
   })
 }
