a0c28a55a97b84db9188bd4e51e3e68b130dc6d7
Nicoletta Farabullini <nicoletta.farabullini@mirai-solutions.com>
2019-05-28 11:21:29 +0200

    Error messsage upon load if Meld is not installed or not functioning properly
    closes #3

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,19 @@
 .onLoad <- function(libname, pkgname) {
-# Stop loading of pkg if Meld is not installed or is encountering issues
-  tryCatch({
-    system2("meld", "--version")},
-    warning = function(w) {
-      stop("Meld is not installed or has some issues. Please follow instructions on README.",
+  # Stop loading of pkg if Meld is not installed or is encountering issues
+  suppressWarnings({
+    status <- system2("meld", "--version")
+    if (status == 127) {
+      stop("
+           Meld is not installed or not added to the path environment variable.
+           Check instructions on README
+           (https://github.com/miraisolutions/compareWith#Installation).",
            call. = FALSE)
-    })
-
+    } else if (status > 0 && status != 127) {
+      stop("
+           Existing Meld is not working correctly.
+           Make sure your Meld installation is fully functional. Check instructions on README
+           (https://github.com/miraisolutions/compareWith#Installation).",
+           call. = FALSE)
+    }
+  })
 }
