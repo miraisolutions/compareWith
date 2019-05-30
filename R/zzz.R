d937fac5adc60086b6a4466c25cc5433053cc37b
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2019-05-30 21:55:57 +0200

    Re-factor Meld version check to capture stdout (#3).
    * Needed to avoid warnings in R CMD check.
    * Extracted as unit-testable function (TODO unit tests).

--- R/zzz.R
+++ R/zzz.R
@@ -1,28 +1,45 @@
+
+.onLoad <- function(libname, pkgname) {
+  # In principle we should not be messaging anything .onLoad(), not even via
+  # packageStartupMessage (NOTE in R CMD CHECK)
+  packageStartupMessage("Check Meld installation:")
+  packageStartupMessage(check_meld_version())
+}
+
 check_readme_msg <- c(
   "Check instructions in the package README\n",
   "<https://github.com/miraisolutions/compareWith#installation>."
 )
 
-.onLoad <- function(libname, pkgname) {
-  # Check Meld version, stop pkg loading if Meld is not installed or there are
-  # issues determining its version
-  cat("Check Meld installation:\n")
-  suppressWarnings({
-    status <- system2("meld", "--version")
-    if (status == 127) {
+# Check Meld version, stop if Meld is not installed or there are issues
+# determining its version. Extra arguments allow mimicking error situations for
+# testing
+check_meld_version <- function(meld = "meld", option = "--version") {
+  out <- withCallingHandlers(
+    suppressWarnings(system2(meld, option, stdout = TRUE)),
+    error = function(e) {
       stop(
         "Unable to detect a Meld installation.\n",
         "Make sure `meld` is installed and can be called from the command line.\n",
         check_readme_msg,
         call. = FALSE
       )
-    } else if (status != 0) {
-      stop(
-        "Existing Meld installation not working correctly.\n",
-        "Make sure you have a fully functional `meld` installed in your system.\n",
-        check_readme_msg,
-        call. = FALSE
-      )
     }
-  })
+  )
+  if (isTRUE(attr(out, "status") != 0)) {
+    message(paste(out, collapse = "\n"))
+    stop(
+      "Existing Meld installation not working correctly.\n",
+      "Make sure you have a fully functional `meld` installed in your system.\n",
+      check_readme_msg,
+      call. = FALSE
+    )
+  }
+  # return the successful result of meld --version
+  out
 }
+
+# TODO: Unit-test
+# check_meld_version() => expect [Mm]eld.*\d\\.\d
+# check_meld_version(meld = "me_ld") => expect_error
+# check_meld_version(option = "--ver_sion") => expect_error
